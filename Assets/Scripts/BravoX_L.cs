c6917e93fab8b2fbce4a012535cf4e0b9348b29a
Capricious-Liu <1073715498@qq.com>
2017-05-15 20:42:18 +0800

    Basic Filter: interface completed

---
+++ Assets/Scripts/BravoX_L.cs
@@ -0,0 +1,133 @@
+﻿/****************************************************
+Script is changed by Liu Zhili depend on Brovo!X Image Color Filter
+Now it`s free for Unity PRO.
+
+****************************************************/
+/****************************************************
+
+Bravo!X Image Color Filter write by Razor Yhang 2014
+	
+# Intro:
+Bravo!X is based on the GNU GPL Open Source Licence. You can change it as you needed. With Bravo!X Filter, you can 
+simply and easily manipulate the color of your final graphics, including brightness, saturation, hue and color 
+balance. All the parameters are Photoshop-like and accessible from other scripts, which means you can easily 
+control it on the go.The API onRenderImage() is used by Bravo!X so this Filter require UNITY PRO.
+		
+# How to use:
+1.Drag the BravoX.cs script to the camera.
+2.That's it:)
+
+p.s:Please drag the shader "BravoXFilter" to the parameter "Shader" in the   BravoX script if the shader is not 
+set by default sometimes. Also, we use a LUT map(look up table)to make things work,
+
+# Notice:
+ This filter is almost done but still in develop(that means it is poorly tested), I have tested it in 4.5.2 / 4.3 
+/ 4.0 with my PC and it works fine. However I don't have environment to test it in MAC or any mobile device. You 
+can change any implement details of it and make it work in your environment and project. And,it would be very 
+helpful if you can tell me some platform unadaptable&failed problems.
+			
+# Questions and sugestions? Please contact: 709230917@qq.com
+
+****************************************************/
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class BravoX_L : MonoBehaviour
+{
+    public Texture2D LookUpRamp;		// Do not change it if you know how it works inside out.	
+    public Shader shader;
+    private Material curMaterial;
+	public float MasterSaturation 		= 1;		// Main switch of the saturation.
+	public float Saturation_Red 		= 1;		// Saturation switch for Red Channel.
+	public float Saturation_Green 		= 1;		// Saturation switch for Green Channel.
+	public float Saturation_Blue 		= 1;		// Saturation switch for Blue Channel.
+													
+	
+	public float MasterBrightness 		= 1;		// Main switch of the Brightness.
+	public float BrightBias_Light 		= 0;		// Brightness bias switch for light part of the image.
+	public float BrightBias_Grey 		= 0;		// Brightness bias switch for middle tone part of the image.
+	public float BrightBias_Dark	    = 0;		// Brightness bias switch for shadow part of the image.
+													// [NOTICE]: Wanna change contrast? Try to mix and match the brightness of
+													// light / middle tone / shadow part of the image to get that.
+
+	public float BlanceRedCyan_Light 	= 0;		// Color Balance of the Red - Cyan (R - GB)channel of the light part in image.
+	public float BlanceGreenMagenta_Light = 0;		// Color Balance of the Green - Magenta (G - RB)channel of the light part in image.
+	public float BlanceBlueYellow_Light = 0;		// Color Balance of the Blue - Yellow (B - RG)channel of the light part in image.
+
+	public float BlanceRedCyan_Dark 	= 0;		// Color Balance of the Red - Cyan (R - GB)channel of the dark part in image.
+	public float BlanceGreenMagenta_Dark = 0;		// Color Balance of the Green - Magenta (G - RB)channel of the dark part in image.
+	public float BlanceBlueYellow_Dark 	= 0;        // Color Balance of the Blue - Yellow (B - RG)channel of the dark part in image.
+
+
+    #region Properties  
+    public Material material
+    {
+        get
+        {
+            if (curMaterial == null)
+            {
+                curMaterial = new Material(shader);
+                material.hideFlags = HideFlags.HideAndDontSave;
+            }
+            return curMaterial;
+        }
+    }
+    #endregion
+
+
+
+    void Start () {
+		if(!shader){
+			shader = Shader.Find("Hidden/BravoX");
+		}
+
+		if(!shader){
+			this.enabled = false;
+		}
+	}
+	// Clamp params for the filter. You can fully change it as you needed.
+	void Update() {
+		MasterSaturation = Mathf.Clamp( MasterSaturation, 0, 4 );
+		Saturation_Red = Mathf.Clamp( Saturation_Red, 0, 5 );
+		Saturation_Green = Mathf.Clamp( Saturation_Green, 0, 5 );
+		Saturation_Blue = Mathf.Clamp( Saturation_Blue, 0, 5 );
+		
+		MasterBrightness = Mathf.Clamp( MasterBrightness, 0, 5 );
+		BrightBias_Light = Mathf.Clamp( BrightBias_Light, -1, 1 );
+		BrightBias_Grey = Mathf.Clamp( BrightBias_Grey, -1, 1 );
+		BrightBias_Dark = Mathf.Clamp( BrightBias_Dark, -1, 1 );
+		
+		BlanceRedCyan_Light = Mathf.Clamp( BlanceRedCyan_Light, -0.5f, 0.5f );
+		BlanceGreenMagenta_Light = Mathf.Clamp( BlanceGreenMagenta_Light, -0.5f, 0.5f );
+		BlanceBlueYellow_Light = Mathf.Clamp( BlanceBlueYellow_Light, -0.5f, 0.5f );
+		BlanceRedCyan_Dark = Mathf.Clamp( BlanceRedCyan_Dark, -0.5f, 0.5f );
+		BlanceGreenMagenta_Dark = Mathf.Clamp( BlanceGreenMagenta_Dark, -0.5f, 0.5f );
+		BlanceBlueYellow_Dark = Mathf.Clamp( BlanceBlueYellow_Dark, -0.5f, 0.5f );
+	}
+
+	// Called by camera to apply image effect.
+    void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+
+		material.SetTexture("LookUpRamp", LookUpRamp );
+
+		material.SetFloat( "Saturation", MasterSaturation );
+		material.SetFloat( "Saturation_r", Saturation_Red );
+		material.SetFloat( "Saturation_g", Saturation_Green );
+		material.SetFloat( "Saturation_b", Saturation_Blue );
+
+		material.SetFloat( "ColBalanceR_l", BlanceRedCyan_Light );
+		material.SetFloat( "ColBalanceG_l", BlanceGreenMagenta_Light );
+		material.SetFloat( "ColBalanceB_l", BlanceBlueYellow_Light );
+		material.SetFloat( "ColBalanceR_d", BlanceRedCyan_Dark );
+		material.SetFloat( "ColBalanceG_d", BlanceGreenMagenta_Dark );
+		material.SetFloat( "ColBalanceB_d", BlanceBlueYellow_Dark );
+
+		material.SetFloat( "Brightness", MasterBrightness );
+		material.SetFloat( "Bright_l", BrightBias_Light );
+		material.SetFloat( "Bright_g", BrightBias_Grey );
+		material.SetFloat( "Bright_d", BrightBias_Dark );
+
+        Graphics.Blit( source, destination, material, 0 );
+    }
+}
---
+++ Assets/Shaders/BravoXFilter.shader
@@ -0,0 +1,97 @@
+﻿Shader "Hidden/BravoX" {
+Properties {
+	_MainTex ("Base (RGB)", RECT) = "white" {}
+	
+	Saturation("Master Saturation", Float) = 1      //饱和度
+	Saturation_r("Saturation(Red)", Float) = 1
+	Saturation_g("Saturation(Green)", Float) = 1
+	Saturation_b("Saturation(Blue)", Float) = 1
+	
+	Brightness("Master Brightness", Float) = 1      //亮度
+	Bright_d("Brightness(Dark)", Float) = 0
+	Bright_g("Brightness(Grey)", Float) = 0
+	Bright_l("Brightness(light)", Float) = 0
+	
+	ColBalanceR_l("RedBalance(light)", Float) = 0   //平衡度
+	ColBalanceG_l("GreenBalance(light)", Float) = 0
+	ColBalanceB_l("BlueBalance(light)", Float) = 0
+	
+	ColBalanceR_d("RedBalance(Dark)", Float) = 0
+	ColBalanceG_d("GreenBalance(Dark)", Float) = 0
+	ColBalanceB_d("BlueBalance(Dark)", Float) = 0
+	
+	LookUpRamp("Base (RGB)", RECT) = "white" {}
+}
+
+SubShader {
+	Pass { // #0 
+		ZTest Always Cull Off ZWrite Off
+		Fog { Mode off }
+
+		CGPROGRAM
+		#pragma vertex vert_img
+		#pragma fragment frag
+		#pragma fragmentoption ARB_precision_hint_fastest
+		#include "UnityCG.cginc"
+
+		uniform sampler2D LookUpRamp;
+		uniform sampler2D _MainTex;
+		
+		uniform float Saturation, Saturation_r, Saturation_g, Saturation_b;
+		uniform float Brightness, Bright_l, Bright_g, Bright_d;
+		uniform float ColBalanceR_l,ColBalanceG_l,ColBalanceB_l;
+		uniform float ColBalanceR_d,ColBalanceG_d,ColBalanceB_d;
+		
+		half4 frag (v2f_img i) : COLOR {
+
+			fixed4 original = tex2D(_MainTex, i.uv);
+			fixed4 o = original;
+			
+			fixed lumi = min (1, Luminance(original));
+			fixed c_l = min(min(original.r,original.g),original.b);
+			
+			ColBalanceR_l >= 0 ?
+				( o.r += o.r * ColBalanceR_l * lumi) :
+				( o.gb += o.gb * -ColBalanceR_l * lumi);
+			
+			ColBalanceG_l >= 0 ?
+				( o.g += o.g * ColBalanceG_l * lumi) :
+				( o.rb += o.rb * -ColBalanceG_l * lumi);
+				
+			ColBalanceB_l >= 0 ?
+				( o.b += o.b * ColBalanceB_l * lumi) :
+				( o.rg += o.rg * -ColBalanceB_l * lumi);
+				
+			ColBalanceR_d >= 0 ?
+				( o.r += o.r * ColBalanceR_d * (1- lumi) ):
+				( o.gb += o.gb * -ColBalanceR_d * (1- lumi) );
+			
+			ColBalanceG_d >= 0 ?
+				( o.g += o.g * ColBalanceG_d * (1- lumi)) :
+				( o.rb += o.rb * -ColBalanceG_d * (1- lumi));
+				
+			ColBalanceB_d >= 0 ?
+				( o.b += o.b * ColBalanceB_d * (1- lumi)) :
+				( o.rg += o.rg * -ColBalanceB_d * (1- lumi));
+				
+			o = lerp(lumi, o, Saturation);
+			o.r = min(1, lerp(c_l, o.r, Saturation_r));
+			o.g = min(1, lerp(c_l, o.g, Saturation_g));
+			o.b = min(1, lerp(c_l, o.b, Saturation_b));
+			
+			o += o * ( ( tex2D(LookUpRamp, fixed2(lumi,.6)))* Bright_l
+			 			+ tex2D(LookUpRamp, fixed2(lumi,.3))*Bright_g
+			  			+ tex2D(LookUpRamp, fixed2(lumi,1))*Bright_d );	
+			o *=  Brightness ;
+			o.rgb = min(fixed3(1,1,1), o.rgb);
+			o.a = original.a;
+		
+			return o;
+		}
+		ENDCG
+	}
+}
+
+Fallback off
+
+}
